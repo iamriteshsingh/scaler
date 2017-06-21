a84a30aff9d157c731a5375ef882ac0b34e08579
iamriteshsingh <riteshsinghrocks123@gmail.com>
2017-06-21 11:49:08 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace scaler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.imageBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(386, 280);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseClick);
+            this.imageBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.imageBox1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(432, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(455, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(432, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(432, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(432, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(432, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(588, 308);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scaler";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace scaler
+{
+    public partial class Form1 : Form
+    {
+        private Boolean flag = false ;
+        private Graphics g;
+        private int x, y;
+        public int cc = 0;
+        private double len,lenu,lenl;
+        private Color c;
+        private int xx ,yy ;
+        private Capture capture;        //takes images from camera as image frames
+        private bool captureInProgress;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+        
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            //Image<Bgr, Byte> ImageFrame = capture.QueryFrame();
+            
+            g.DrawRectangle(new Pen(Color.GreenYellow), 100,60, 170, 170);
+            imageBox1.Image = capture.QueryFrame();
+           
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                
+                try
+                {
+                    capture = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    button1.Text = "Start!"; //
+                   
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    button1.Text = "Capture";
+                    Application.Idle += ProcessFrame;
+                    g = imageBox1.CreateGraphics();
+                    g.DrawRectangle(new Pen(Color.GreenYellow), 100, 60, 170, 170);
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            if (cc == 0)
+            {
+                x = e.X;
+                y = e.Y;
+                lenu =  Math.Sqrt(((184 - x) * (184 - x)) + ((61 - y) * (61 - y)));
+                
+                g.DrawLine(new Pen(Color.Gold), xx, yy, x, y);
+                g.DrawEllipse(new Pen(Color.Blue), x, y, 5, -5);
+                cc = 1;
+            }
+            if(cc == 1 )
+            {
+                
+                xx = e.X;
+                yy = e.Y;
+                lenl = Math.Sqrt(((184 - xx) * (184 - xx)) + ((231 - yy) * (231 - yy)));
+                
+                g.DrawLine(new Pen(Color.Gold), xx, yy, x, y);
+                g.DrawEllipse(new Pen(Color.Red), xx, yy, 5, -5);
+                label1.Visible = true;
+                len = Math.Sqrt(((xx - x) * (xx - x)) + ((yy - y) * (yy - y)));
+                label1.Text  = Convert.ToString(len);
+                button2.Visible = true;
+               
+            }
+               
+            
+        }
+
+        private void imageBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                flag = true;
+            }
+            xx = e.X;
+            yy = e.Y;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            len = len / 40;
+            //MessageBox.Show("img len " + Convert.ToString(len));
+            //double lu = ((lenu* 15) / (lenu - 15)*5);
+            //double ll = ((lenl * 15) / (lenl - 15)*5) ;
+            lenu = lenu / 5; 
+            lenl = lenl / 5;
+            //MessageBox.Show("ln u : " + lenu);
+            //MessageBox.Show("ln l : " + lenl);
+            double m ,l;
+            if (lenu > lenl)
+            {
+                l = lenu * 5;
+                m = lenu;
+            }
+            else
+            {
+                l = lenl * 5;
+                m = lenl;
+            }
+            //m = 1 / (0.2 - (1 / l));
+            //MessageBox.Show(Convert.ToString("image dis " + m));
+            //MessageBox.Show("distance of object "+Convert.ToString(l));
+            double mag = m / l ;
+            //MessageBox.Show(Convert.ToString("mag : " + mag));
+            double h = len / mag ;
+            //MessageBox.Show(" height  " + Convert.ToString(h));
+            label2.Visible = true; label3.Visible = true; label4.Visible = true; label5.Visible = true;
+            len = Math.Round(len, 2); l = Math.Round(l, 2); h = Math.Round(h, 2);
+            label4.Text = "Height of Image: " + Convert.ToString(len) + " cm";
+            label2.Text = "Distance of object: " + Convert.ToString(l) + " cm";
+            label3.Text = "Height of object: " + Convert.ToString(h) + " cm";
+            label5.Text = "Error +-2cm ";
+            //double
+           // MessageBox.Show(Convert.ToString(l));
+        }
+    }
+}
